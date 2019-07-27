830961e947e73cfbaa48de901e60d86756c6847d
danwiltshire <me@danielwiltshire.co.uk>
2019-07-27 09:09:41 +0100

    content

--- app/components/BigKahunaBurger/BigKahunaBurger.js
+++ app/components/BigKahunaBurger/BigKahunaBurger.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const BigKahunaBurger = () => (
-    <h1>BigKahunaBurger</h1>
+    <div>
+        
+        <header>
+
+            <h1>BigKahunaBurger</h1>
+            <p>Hey, nice to meet you!</p>
+
+            <p>I’m an IT Infrastructure Security Administrator for apetito UK where I implement strong and usable IT security solutions. In my spare time, I play a variety of instruments, shoot photographs, design websites and build my skill-set in my home lab.</p>
+
+            <a href="https://www.linkedin.com/in/danielwiltshire/">My LinkedIn</a>
+
+        </header>
+
+        <main>
+
+            <h2>Photography</h2>
+
+            <article>
+                <img
+                    src="https://danielwiltshire.co.uk/images/photography/louisiana_design_museum_1_small.jpg"
+                    alt="Looking Glass"
+                />
+            </article>
+
+        </main>
+
+    </div>
 )
 
 export default BigKahunaBurger
