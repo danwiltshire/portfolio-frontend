4bf0ed7206451e2ae962d3bc0c2fd922e7e842a8
danwiltshire <me@danielwiltshire.co.uk>
2019-07-27 09:28:28 +0100

    linting

--- app/components/BigKahunaBurger/BigKahunaBurger.js
+++ app/components/BigKahunaBurger/BigKahunaBurger.js
@@ -1,51 +1,53 @@
 import React from 'react';
 
 const BigKahunaBurger = () => (
-    <div>
-        
-        <header>
 
-            <h1>BigKahunaBurger</h1>
-            <p>Hey, nice to meet you!</p>
+  <div>
 
-            <p>I’m an IT Infrastructure Security Administrator for apetito UK where I implement strong and usable IT security solutions. In my spare time, I play a variety of instruments, shoot photographs, design websites and build my skill-set in my home lab.</p>
+    <header>
 
-            <a
-                href="https://www.linkedin.com/in/danielwiltshire/"
-                title="See my profile on LinkedIn">
-                LinkedIn Profile
-            </a>
+      <h1>BigKahunaBurger</h1>
+      <p>Hey, nice to meet you!</p>
 
-        </header>
+      <p>I’m an IT Infrastructure Security Administrator for apetito UK where I implement strong and usable IT security solutions. In my spare time, I play a variety of instruments, shoot photographs, design websites and build my skill-set in my home lab.</p>
 
-        <main>
+      <a
+        href="https://www.linkedin.com/in/danielwiltshire/"
+        title="See my profile on LinkedIn">
+        LinkedIn Profile
+      </a>
 
-            <h2>Photography</h2>
+    </header>
 
-            <article>
-                <img
-                    src="https://danielwiltshire.co.uk/images/photography/louisiana_design_museum_1_small.jpg"
-                    alt="Looking Glass at Louisiana Design Museum, Copenhagen"
-                />
-            </article>
+    <main>
 
-            <article>
-                <img
-                    src="https://danielwiltshire.co.uk/images/photography/sankt_jorgens_so_sunset_small.jpg"
-                    alt="Sankt Jørgens Sø at sunset, Copenhagen"
-                />
-            </article>
+      <h2>Photography</h2>
 
-            <article>
-                <img
-                    src="https://danielwiltshire.co.uk/images/photography/louisiana_design_museum_2_small.jpg"
-                    alt="Museum Gardens at Louisiana Design Museum, Copenhagen"
-                />
-            </article>
+      <article>
+        <img
+          src="https://danielwiltshire.co.uk/images/photography/louisiana_design_museum_1_small.jpg"
+          alt="Looking Glass at Louisiana Design Museum, Copenhagen"
+        />
+      </article>
 
-        </main>
+      <article>
+        <img
+          src="https://danielwiltshire.co.uk/images/photography/sankt_jorgens_so_sunset_small.jpg"
+          alt="Sankt Jørgens Sø at sunset, Copenhagen"
+        />
+      </article>
 
-    </div>
+      <article>
+        <img
+          src="https://danielwiltshire.co.uk/images/photography/louisiana_design_museum_2_small.jpg"
+          alt="Museum Gardens at Louisiana Design Museum, Copenhagen"
+        />
+      </article>
+
+    </main>
+
+  </div>
+
 )
 
 export default BigKahunaBurger
--- app/index.js
+++ app/index.js
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import BigKahunaBurger from './components/BigKahunaBurger'
 
-// ES6 class component
-/* class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>Hello World</p>
-                <Test />
-            </div>
-            
-        )
-    }
-} */
-
-/* const BBC = () => (
-    <div>Hello</div>
-) */
-
 // JavaScript standard functional
-function App() {
-    return <BigKahunaBurger />;
+function App () {
+  return <BigKahunaBurger />
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
