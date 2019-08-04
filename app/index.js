04926359eeab7de56bb6994a3aff3484a6009e8c
Daniel Wiltshire <me@danielwiltshire.co.uk>
2019-08-04 10:59:09 +0100

    working polyfill

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import 'promise-polyfill/src/polyfill' // IE compatibility
 import BigKahunaBurger from './components/BigKahunaBurger'
 
 // JavaScript standard functional
