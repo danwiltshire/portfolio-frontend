3fe02c7e37c40b5760f8b352610d1dc7743d1ae9
Daniel Wiltshire <me@danielwiltshire.co.uk>
2019-08-04 09:59:54 +0100

    API component

---
+++ app/utils/API/API.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: "https://895o1wozfh.execute-api.eu-west-2.amazonaws.com/prod/",
+  responseType: "json"
+})
