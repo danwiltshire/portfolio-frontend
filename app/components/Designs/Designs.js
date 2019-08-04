050237951635e392106095ed35a44ba431d3564c
Daniel Wiltshire <me@danielwiltshire.co.uk>
2019-08-04 11:02:17 +0100

    comment cleanup

--- app/components/Designs/Designs.js
+++ app/components/Designs/Designs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import axios from 'axios'
 import API from "../../utils/API";
 
 export default class Designs extends React.Component {
--- app/components/Photographs/Photographs.js
+++ app/components/Photographs/Photographs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import axios from 'axios'
 import API from "../../utils/API";
 
 export default class Photographs extends React.Component {
