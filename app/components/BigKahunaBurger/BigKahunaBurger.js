c4c1d4ffae567220c09d64557abfb4fa63094a20
Daniel Wiltshire <me@danielwiltshire.co.uk>
2019-08-25 15:47:40 +0100

    linting

--- app/components/BigKahunaBurger/BigKahunaBurger.js
+++ app/components/BigKahunaBurger/BigKahunaBurger.js
@@ -11,35 +11,29 @@ const BigKahunaBurger = () => (
         <tr>
           <td>
 
+            <div className="title-with-subtitle">
+              <h1>Daniel Wiltshire</h1>
+              <h3>Specialist in IT Security</h3>
+            </div>
 
-          <div className="title-with-subtitle">
-        <h1>Daniel Wiltshire</h1>
-        <h3>Specialist in IT Security</h3>
-      </div>
-
-      <div className="button">
-        <a
-          href="https://www.linkedin.com/in/danielwiltshire/"
-          title="See my profile on LinkedIn">
-          LinkedIn
-        </a>
-      </div>
+            <div className="button">
+              <a
+                href="https://www.linkedin.com/in/danielwiltshire/"
+                title="See my profile on LinkedIn">
+                LinkedIn
+              </a>
+            </div>
 
           </td>
           <td>
 
+            <p>I’m a Service Delivery Engineer for NCR where I automate the delivery of mission-critical SaaS applications worldwide.</p>
 
-            
-      <p>I’m a Service Delivery Engineer for NCR where I automate the delivery of mission-critical SaaS applications worldwide.</p>
-
-<p>In my spare time I play a variety of instruments, shoot photos, design web apps and build my skillset in a home lab.</p>
-
+            <p>In my spare time I play a variety of instruments, shoot photos, design web apps and build my skillset in a home lab.</p>
 
           </td>
         </tr>
       </table>
-
-
 
     </header>
 
