358d2da7b9c9b4a43dfded1717addf3711471db2
Daniel Wiltshire <me@danielwiltshire.co.uk>
2019-08-30 19:56:32 +0100

    button links

--- app/components/BigKahunaBurger/BigKahunaBurger.js
+++ app/components/BigKahunaBurger/BigKahunaBurger.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { Header, Segment, Container, Image, Button, Icon, List } from 'semantic-ui-react'
+import { Header, Segment, Label, Container, Image, Button, Icon, List } from 'semantic-ui-react'
 
 import Designs from '../Designs'
 import Photographs from '../Photographs'
 
 const BigKahunaBurger = () => (
-  <Container text>
+  <Container id="main" text>
 
     <Segment basic>
       <Header as='h1'>
@@ -16,9 +16,11 @@ const BigKahunaBurger = () => (
         </Header.Subheader>
       </Header>
       <p>I’m a Service Delivery Engineer for NCR where I automate the delivery of mission-critical SaaS applications worldwide.</p>
-      <Button color='linkedin'>
-        <Icon name='linkedin' /> LinkedIn
-      </Button>
+      <a href="https://www.linkedin.com/in/danielwiltshire/">
+        <Button color='linkedin'>
+          <Icon name='linkedin' /> LinkedIn
+        </Button>
+      </a>
     </Segment>
 
     <Segment basic>
@@ -34,32 +36,34 @@ const BigKahunaBurger = () => (
 
     <Segment basic>
       <Header as='h2'>Photography</Header>
-      <Image src='https://s3.eu-west-2.amazonaws.com/media.danielwiltshire.co.uk/images/photography/sankt_jorgens_so_dusk_small.jpg' fluid rounded />
-      <p>Sankt Jorgens So, Copenhagen</p>
-      <Button>
-        Load more (6)
-      </Button>
+      <div className="gallery-image">
+        <Image src='https://s3.eu-west-2.amazonaws.com/media.danielwiltshire.co.uk/images/photography/sankt_jorgens_so_dusk_small.jpg' fluid rounded />
+        <span>Sankt Jørgens Sø, Copenhagen</span>
+      </div>
     </Segment>
 
     <Segment basic>
       <Header as='h2'>UI design</Header>
-      <Image src='https://s3.eu-west-2.amazonaws.com/media.danielwiltshire.co.uk/images/design/aviation_festival_small.jpg' fluid rounded />
-      <p>Sankt Jorgens So, Copenhagen</p>
-      <Button>
-        Load more (6)
-      </Button>
+      <div className="gallery-image">
+        <Image src='https://s3.eu-west-2.amazonaws.com/media.danielwiltshire.co.uk/images/design/aviation_festival_small.jpg' fluid rounded />
+        <span>Bristol Aviation Fair poster</span>
+      </div>
     </Segment>
 
     <Segment basic>
       <Header as='h2'>Contact</Header>
       <p>Please visit me over at LinkedIn.</p>
-      <Button color='linkedin'>
-        <Icon name='linkedin' /> LinkedIn
-      </Button>
+      <a href="https://www.linkedin.com/in/danielwiltshire/">
+        <Button color='linkedin'>
+          <Icon name='linkedin' /> LinkedIn
+        </Button>
+      </a>
     </Segment>
 
-    <Segment basic>
+    <Segment id="footer" basic>
       <p>This serverless app is written in JavaScript and is continuously deployed to AWS using Travis CI.</p>
+
+      <p><a href="https://github.com/danwiltshire/portfolio-frontend">Source code on GitHub</a></p>
     </Segment>
 
   </Container>
