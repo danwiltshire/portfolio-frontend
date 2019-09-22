a6d9f86c489a75570d6a8fb80597999bb25dd621
danielwhitford <whitford.dsw@gmail.com>
2019-09-22 13:37:18 +0100

    Add in router PoC

--- app/index.js
+++ app/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import 'promise-polyfill/src/polyfill' // IE compatibility
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
-import BigKahunaBurger from './components/BigKahunaBurger'
+import AppRouter from './components/AppRouter'
 
 // JavaScript standard functional
 function App () {
-  return <BigKahunaBurger />
+  return <AppRouter />
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
