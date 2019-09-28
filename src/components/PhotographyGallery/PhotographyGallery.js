76676a160746cef3899bcb0cbce3efeb7fdbb575
danielwhitford <whitford.dsw@gmail.com>
2019-09-28 20:23:51 +0100

    icon

--- src/components/PhotographyGallery/PhotographyGallery.js
+++ src/components/PhotographyGallery/PhotographyGallery.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import API from '../../utils/API'
-import { Header, Grid, Container, Image, Sticky, Dimmer, Loader } from 'semantic-ui-react'
+import { Header, Icon, Grid, Container, Image, Sticky, Dimmer, Loader } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 export default class PhotographyGallery extends React.Component {
